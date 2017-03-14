85b907d7c7e1875dd58473e0012e1b9ebccd3f7c
Jack Liu <lw@dasudian.com>
2017-03-14 20:26:42 +0800

    Update demo-java && README.md

--- README.md
+++ README.md
@@ -1,15 +1,13 @@
-# Dasudian IoT DataHub Android Example
+# Dasudian IoT DataHub Java SDK demo
 
-Example Android application using the IoT DataHub Java SDK
-
-Please put the `iot-datahub-sdk-java-x.x.x.jar` into the `libs/` folder.
+Please put the `dasudian-datahub-sdk.x.x.x.jar` into the `libs/` folder.
 
 ## 目录结构说明
 
 ### demo目录
 该目录下有使用SDK的各种demo.
 
-[demo](./demo/src/com/dasudian/iot_datahub_sdk_demo_android/MainActivity.java)
+[Android demo](./demo/src/com/dasudian/iot_datahub_sdk_demo_android/MainActivity.java)
 
 [java demo1](./demo-java/Main.java)
 
@@ -22,3 +20,8 @@ Please put the `iot-datahub-sdk-java-x.x.x.jar` into the `libs/` folder.
 该目录下有关于SDK的文档
 
 [API文档](./docs/API.md)
+
+## 编译和运行demo
+Android demo使用eclipse开发，也可以将起导入到Android Studio运行。
+
+## Q&A
--- demo-java/Main.java
+++ demo-java/Main.java
@@ -1,5 +1,11 @@
+/*
+ * Licensed Materials - Property of Dasudian 
+ * Copyright Dasudian Technology Co., Ltd. 2016-2017 
+ */
 package com.dasudian.iot.demo;
 
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
 import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -7,12 +13,17 @@ import java.util.logging.Logger;
 import com.dasudian.iot.sdk.ActionCallback;
 import com.dasudian.iot.sdk.DataHubClient;
 import com.dasudian.iot.sdk.Message;
-import com.dasudian.iot.sdk.Topic;
+import com.dasudian.iot.sdk.ServiceException;
 
 public class Main {
 	private static final Logger LOGGER = Logger.getLogger(Main.class.getSimpleName());
+	private static final SimpleDateFormat DF = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
 
-	private static class MyCallback implements ActionCallback {
+	static {
+		DF.setTimeZone(TimeZone.getTimeZone("Asia/Shanghai"));
+	}
+
+	private static class MyCallback extends ActionCallback {
 
 		@Override
 		public void onMessageReceived(String topic, byte[] payload) {
@@ -20,39 +31,44 @@ public class Main {
 		}
 
 		@Override
-		public void onPublishSuccess(Message m) {
-			LOGGER.info("onPublishSucceess, topic= " + m.getTopic().getName() + ",content= " + m.getPayload());
+		public void onConnectionStatusChanged(boolean isConnected) {
+			LOGGER.info("current connect status = " + isConnected);
 		}
+	}
 
-		@Override
-		public void onPublishFailure(Message m, Throwable t) {
-			LOGGER.info("onPublishFailure");
-		}
-
-		@Override
-		public void connectionLost(Throwable t) {
-			LOGGER.info("connectionLost");
+	private static boolean sub(String topic, DataHubClient client) {
+		try {
+			client.subscribe(topic, 10);
+			LOGGER.info("subscribe success");
+			return true;
+		} catch (ServiceException e) {
+			e.printStackTrace();
+			LOGGER.info("subscribe failed");
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e1) {
+			}
+			return false;
 		}
 	}
 
 	public static void main(String[] args) {
-		// test environment
-		String serverURL = "ssl://try.iotdatahub.net:8883";
 		String instanceId = "dsd_9FmYSNiqpFmi69Bui0_A";
 		String instanceKey = "238f173d6cc0608a";
-		String userName = UUID.randomUUID().toString();
-		String clientId = userName;
-		
+
+		String clientName = UUID.randomUUID().toString();
+		String clientId = clientName;
+
 		try {
-			DataHubClient client = new DataHubClient.Builder(instanceId, instanceKey, userName, clientId).setCallback(
-					new MyCallback()).setServerURI(serverURL).setIgnoreCertificate(true).build();
-			client.connect();
-			Topic topic = new Topic("topic", 2);
-			client.subscribe(topic);
+			DataHubClient client = new DataHubClient.Builder(instanceId, instanceKey, clientName, clientId)
+					.setCallback(new MyCallback()).build();
+			String topic = "topic";
+			while (sub(topic, client) != true)
+				;
 			while (true) {
 				try {
-					Message msg = new Message(topic, "this is message content".getBytes(), 10000);
-					client.sendRequest(msg);
+					Message msg = new Message("this is message content".getBytes());
+					client.sendRequest(topic, msg, 2, 10);
 					LOGGER.info("sent request success");
 					Thread.sleep(2000);
 				} catch (Exception e) {
@@ -64,6 +80,7 @@ public class Main {
 				}
 			}
 		} catch (Exception e) {
+			e.printStackTrace();
 			LOGGER.log(Level.SEVERE, e.getLocalizedMessage(), e);
 		}
 	}
